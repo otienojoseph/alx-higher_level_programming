b79b136e2604c37d79e8e45719b2a9c978bde3e9
otienojoseph <otienoj.cruz@gmail.com>
2023-10-31 17:26:34 +0300

    Refactor: Add new code

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[39:-62]} with Python")
+str = str.split(' ')
+print(f"{str[5]} {str[6]} with Python", end="\n")
