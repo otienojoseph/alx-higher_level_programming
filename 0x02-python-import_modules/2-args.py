084e6663642fd27036cd84f4c2409d723e931e68
otienojoseph <otienoj.cruz@gmail.com>
2023-11-02 19:21:45 +0300

    Feat: Add 2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from sys import argv 
+
+if __name__ == "__main__":
+    arg_len = len(argv)
+    
+    if arg_len < 2:
+        print("0 arguments.")
+    elif arg_len == 2:
+        print("1 argument.")
+    else:
+        print("{} arguments:".format(arg_len - 1))
+
+    for i in range(1, arg_len):
+        print("{}: {}".format(i, argv[i]))
