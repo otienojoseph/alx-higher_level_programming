f91802129df08dee75faf127558f5a04e09a80b4
otienojoseph <otienoj.cruz@gmail.com>
2023-11-06 17:27:46 +0300

    Refactor: Reverse list using slicing

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for num in my_list:
+    reversed_list = my_list[::-1]
+    for num in reversed_list:
         print("{:d}".format(num))
