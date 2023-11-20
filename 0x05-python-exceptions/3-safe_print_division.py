1f00316c453447f167946d1298ebfe691890795b
otienojoseph <otienoj.cruz@gmail.com>
2023-11-20 23:20:35 +0300

    Fix: Add colon on print

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,4 +8,4 @@ def safe_print_division(a, b):
     except ZeroDivisionError as err:
         return None
     finally:
-        print("Inside result {}".format(result))
+        print("Inside result: {}".format(result))
