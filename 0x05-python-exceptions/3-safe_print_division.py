e75c33e062cce2992cf6ba1851c99b06937c243e
otienojoseph <otienoj.cruz@gmail.com>
2023-11-20 22:49:36 +0300

    Add 3-safe_print_division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / float(b)
+        return result
+    except ZeroDivisionError as err:
+        return None
+    finally:
+        print("Inside result {}".format(result))
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -2,3 +2,4 @@
 - 0-safe_print_list: Prints x elements of a list
 - 1-safe_print_integer: Prints an integer
 - 2-safe_print_list_integers: Print the first x elements of a list and only integers
+- 3-safe_print_division: Function that divides 2 integers and prints the result
