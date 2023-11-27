9ece819f0046aea41435cf4b07bfa45c1f4dc79c
otienojoseph <otienoj.cruz@gmail.com>
2023-11-27 15:14:41 +0300

    Fix: Use isinstance to check type

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,6 +6,10 @@
 class Rectangle:
     """
         Rectangle class attributes
+
+        Attributes:
+            __width (int): The width of the rectangle
+            __height (int): The height of the rectangle
     """
     def __init__(self, width=0, height=0):
         self.__width = width
@@ -13,11 +17,21 @@ class Rectangle:
 
     @property
     def width(self):
+        """Retrieve the width of the rectangle"""
         return self.width
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        """Set the width of the rectangle.
+
+            Args:
+                value (int): The width to set.
+
+            Raises:
+                TypeError: If width is not an integer.
+                ValueError: If width is less than 0.
+        """
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -26,11 +40,21 @@ class Rectangle:
 
     @property
     def height(self):
+        """Retrieve the height of the rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        """Set the height of the rectangle.
+
+            Args:
+                value (int): The height to set.
+
+            Raises:
+                TypeError: If height is not an integer.
+                ValueError: If height is less than 0.
+        """
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
