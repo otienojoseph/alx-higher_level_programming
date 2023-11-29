69547831886e7c747200c1473587dfad90f725cb
otienojoseph <otienoj.cruz@gmail.com>
2023-11-29 16:33:34 +0300

    Fix: Define optional private feilds the correct way

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
             __height (int): The height of the rectangle
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
