046ef3fb02f3458e72bc5e309f1a8de1c7e48e34
otienojoseph <otienoj.cruz@gmail.com>
2024-01-02 10:05:55 +0300

    Fix: Return result as integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,12 +15,10 @@ def add_integer(a, b=98):
         Raises:
             TypeError - if either parameters is not an integer
     """
-    if not isinstance(a, (int, float)):
+    if a is None or not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    a = int(a)
-    b = int(b)
-
-    return a + b
+    result = a + b
+    return int(result)
