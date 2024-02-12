e85a691c23b588dc45c5d19bf8e748f251dc6708
otienojoseph <otienoj.cruz@gmail.com>
2024-02-13 01:14:40 +0300

    fix: Check if number is integer

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,8 +7,15 @@ const numbers = args.map(Number).filter(num => Number.isInteger(num));
 if (numbers.length < 2) {
   console.log(0);
 } else {
-  numbers.sort((a, b) => b - a);
-  const secondBiggest = numbers[1];
+  let max = -Infinity; let result = -Infinity;
 
-  console.log(secondBiggest);
+  for (const value of numbers) {
+    if (value > max) {
+      [result, max] = [max, value];
+    } else if (value < max && value > result) {
+      result = value;
+    }
+  }
+
+  console.log(result);
 }
