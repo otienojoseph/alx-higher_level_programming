0a35bfbdfc575e7f9fd4cc0e9aaa9ea1b3f2caf3
otienojoseph <otienoj.cruz@gmail.com>
2024-02-14 23:43:40 +0300

    fix: Return empty object

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,8 @@
 class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return {};
+        this.width;
+        this.height;
     } else {
       this.width = w;
       this.height = h;
