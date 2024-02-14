9844e0d6d474401bbe1ccff910580ab7dd13a66f
otienojoseph <otienoj.cruz@gmail.com>
2024-02-15 00:29:30 +0300

    style: Add semistandard rules

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,30 +1,30 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
-      let row = "";
+      let row = '';
       for (let j = 0; j < this.width; j++) {
-        row += "X";
+        row += 'X';
       }
       console.log(row);
     }
   }
 
-  rotate() {
-    let temp = this.width;
+  rotate () {
+    const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
 
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
