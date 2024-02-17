9aa24a92aed5bfbed7f503a53da0a5660c81ce7c
otienojoseph <otienoj.cruz@gmail.com>
2024-02-17 16:26:44 +0300

    refactor: Convert using toString

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,17 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  if (base < 2 || base > 36) {
-    return "Invalid choice";
-  }
-
   return function (number) {
-    this.result = "";
-    while (number > 0) {
-      this.result = (number % base).toString(base) + this.result;
-      number = Math.floor(number / base);
-    }
-
-    return this.result;
+    return number.toString(base);
   };
 };
