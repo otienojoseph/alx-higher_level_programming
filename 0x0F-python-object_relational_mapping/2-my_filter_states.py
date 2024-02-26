0bf1bdb5793dcc8e8eaae153a625afc74e4206e2
otienojoseph <otienoj.cruz@gmail.com>
2024-02-26 13:40:08 +0300

    Add 2-my_filter_states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+    A script that takes a list of arguments and displays all values
+    in the states table of hbtn_0e_0_usa
+    where name matches the argument
+    username, password, database and state name are given as args.
+"""
+
+import sys
+import MySQLdb
+
+
+def listStates(username, password, db_name, state_name):
+    db = cur = None
+    try:
+        db = MySQLdb.connect(user=username, passwd=password, db=db_name)
+
+        cur = db.cursor()
+        cur.execute("USE %s" % db_name)
+        cur.execute("SELECT * FROM states \
+            WHERE name = %s \
+            ORDER BY id ASC", (state_name,))
+        state = cur.fetchone()
+
+        print(state)
+
+    except MySQLdb.Error as e:
+        print("Error connecting to the MySQL database: ", e)
+
+    finally:
+        if cur:
+            cur.close()
+        if db:
+            db.close()
+
+
+if __name__ == "__main__":
+    try:
+        [username, password, db_name, state_name] = sys.argv[1:]
+        listStates(username, password, db_name, state_name)
+    except ValueError as e:
+        print("Usage: <function-name> user password db_name state_name")
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -1,3 +1,4 @@
 # Python - Object-relational mapping
 - 0-select_states - Script that lists all states from the database hbtn_0e_0_usa
 - 1-filter_states - Script that lists states which start with 'N'
+- 2-my_filter_states - Script that takes an argument and displays all values in the states table
