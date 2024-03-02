84c36268b23d9f083087d18f128cf15c6d630eeb
otienojoseph <otienoj.cruz@gmail.com>
2024-03-02 05:23:57 +0300

    Add 0-body_size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Script that takes in a URL, sends a request to that URL
+# and displays the size of the body of response
+
+size=$(curl -s -o /dev/null -w "%{size_download}" $@)
+echo ${size}
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,2 @@
+# Python - Network 0
+- 0-body_size - Take a URL, send request to URL and display the size of the body of response
