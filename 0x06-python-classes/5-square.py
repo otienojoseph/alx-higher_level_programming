5e99bee7fba40b3c3db4b9b88afd28d687d0df49
otienojoseph <otienoj.cruz@gmail.com>
2024-03-31 14:58:10 +0300

    fix: Add check for size = 0

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -54,6 +54,9 @@ class Square:
 
     def my_print(self):
         """Print to stdout the square with the character '#'"""
+        if self.__size == 0:
+            print()
+
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end="")
