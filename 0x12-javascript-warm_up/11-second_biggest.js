de549ad40eae1cccf85dac768a7af74db1073423
otienojoseph <otienoj.cruz@gmail.com>
2024-04-08 18:17:40 +0300

    fix: Add check for empty array

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,9 @@ const args = process.argv.slice(2);
 const argsInt = args.map(a => parseInt(a));
 
 function secondBiggest (a) {
+  if (a.length === 0) {
+    return 0;
+  }
   // come back to this to optimize
   const b = a.sort();
   return b.at(-2);
