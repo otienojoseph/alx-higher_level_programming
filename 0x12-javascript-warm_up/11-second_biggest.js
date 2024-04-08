040b79f96958943db8304c589de8a6c3d94752c1
otienojoseph <otienoj.cruz@gmail.com>
2024-04-09 00:51:31 +0300

    refacor: Use helper functions

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -9,21 +9,22 @@ function secondBiggest (a) {
   }
 
   // Optimized version
-  // let sortedArr = a.toSorted((a, b) => a - b);
-  // return sortedArr.at(-2)
+  const sortedArr = a.toSorted((a, b) => a - b);
+  return sortedArr.at(-2);
 
   // manual iteration
-  let temp;
-  for (let i = 0; i < a.length - 1; i++) {
-    for (let j = 0; j < a.length - 1 - i; j++) {
-      if (a[j] > a[j + 1]) {
-        temp = a[j];
-        a[j] = a[j + 1];
-        a[j + 1] = temp;
-      }
-    }
-  }
-  return a.at(-2);
+  // let temp;
+  // for (let i = 0; i < a.length - 1; i++) {
+  //     for (let j = 0; j < a.length - 1 - i; j++) {
+  //         if (a[j] > a[j + 1]) {
+  //             temp = a[j];
+  //             a[j] = a[j + 1];
+  //             a[j + 1] = temp;
+  //
+  //         }
+  //     }
+  // }
+  // return a.at(-2);
 }
 
 console.log(secondBiggest(argsInt));
