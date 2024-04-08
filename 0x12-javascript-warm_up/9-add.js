253f2c42cc836c0f2a198e8a8c08219ca5ed7add
otienojoseph <otienoj.cruz@gmail.com>
2024-04-08 17:22:11 +0300

    style: Add corret format

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,8 +2,8 @@
 
 const args = process.argv;
 
-function add(a, b) {
-    return Number(a) + Number(b);
+function add (a, b) {
+  return Number(a) + Number(b);
 }
 
 console.log(add(args[2], args[3]));
