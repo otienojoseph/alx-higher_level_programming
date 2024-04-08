acb096535835661d4cf306c7fc310214c99aa558
otienojoseph <otienoj.cruz@gmail.com>
2024-04-08 17:42:05 +0300

    fix: Use function instead of es6

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,12 +2,12 @@
 
 const args = process.argv;
 
-const factorial = (a) => {
+function factorial (a) {
   const b = Number(a);
   if (b < 1 || isNaN(b)) {
     return 1;
   }
   return b * factorial(b - 1);
-};
+}
 
 console.log(factorial(args[2]));
