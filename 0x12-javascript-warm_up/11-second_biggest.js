48a939ab61ba79cd3ebfa8ab4513e51107d6cf7d
otienojoseph <otienoj.cruz@gmail.com>
2024-04-08 23:21:10 +0300

    refactor: Iterate manually

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,9 +7,17 @@ function secondBiggest (a) {
   if (a.length <= 1) {
     return 0;
   }
-  // come back to this to optimize
-  const b = a.sort();
-  return b.at(-2);
+
+  let temp;
+  // manual, can be optimized
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < a.length; j++) {
+      if (j > i) {
+        temp = j;
+      }
+    }
+  }
+  return a[temp - 1];
 }
 
 console.log(secondBiggest(argsInt));
