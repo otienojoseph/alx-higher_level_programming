efafcd6879a70b720746e53aff9e2de8195b10ba
otienojoseph <otienoj.cruz@gmail.com>
2024-04-09 01:07:22 +0300

    refactor: Refactor code to use single loop

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,30 +1,24 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-const argsInt = args.map(a => parseInt(a));
+const args = process.argv.slice(2).map(Number);
 
 function secondBiggest (a) {
   if (a.length <= 1) {
     return 0;
   }
 
-  // Optimized version
-  const sortedArr = a.toSorted((a, b) => a - b);
-  return sortedArr.at(-2);
+  let largest = args[0];
+  let secondLargest = args[1];
 
-  // manual iteration
-  // let temp;
-  // for (let i = 0; i < a.length - 1; i++) {
-  //     for (let j = 0; j < a.length - 1 - i; j++) {
-  //         if (a[j] > a[j + 1]) {
-  //             temp = a[j];
-  //             a[j] = a[j + 1];
-  //             a[j + 1] = temp;
-  //
-  //         }
-  //     }
-  // }
-  // return a.at(-2);
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] > largest) {
+      secondLargest = largest;
+      largest = args[i];
+    } else if (args[i] > secondLargest && args[i] !== largest) {
+      secondLargest = args[i];
+    }
+  }
+  return secondLargest;
 }
 
-console.log(secondBiggest(argsInt));
+console.log(secondBiggest(args));
