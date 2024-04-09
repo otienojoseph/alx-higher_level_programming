58f283a9dbc812a351986fd2d20c56d3b1cafe61
otienojoseph <otienoj.cruz@gmail.com>
2024-04-09 18:46:24 +0300

    fix: Check for parameters are empty

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,8 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w < 1 || h < 1 || !h || !w) {
-      return {};
+    if (w < 1 || h < 1 || !Number.isInteger(h) || !Number.isInteger(w)) {
+
     } else {
       this.width = w;
       this.height = h;
