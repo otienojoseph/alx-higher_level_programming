92877ec5cff1c983ebae901930095d08b2115adb
otienojoseph <otienoj.cruz@gmail.com>
2024-04-09 23:22:46 +0300

    fix: Check if c equal undefined

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,23 +3,23 @@
 const Square5 = require('./5-square');
 
 class Square extends Square5 {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c) {
-    for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        if (c) {
-          row += 'C';
-        } else {
-          row += 'X';
-        }
-      }
-      console.log(row);
+    constructor(size) {
+        super(size, size);
     }
-  }
+
+    charPrint(c) {
+        for (let i = 0; i < this.height; i++) {
+            let row = '';
+            for (let j = 0; j < this.width; j++) {
+                if (c === undefined) {
+                    row += 'X';
+                } else {
+                    row += 'C';
+                }
+            }
+            console.log(row);
+        }
+    }
 }
 
 module.exports = Square;
