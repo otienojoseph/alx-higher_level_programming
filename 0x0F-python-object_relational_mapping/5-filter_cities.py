2de564fe63a89e4dfc168b60819f1682e264d22a
otienojoseph <otienoj.cruz@gmail.com>
2024-04-13 23:39:02 +0300

    fix: Use INNER JOIN instead

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,27 +6,33 @@ import MySQLdb
 
 
 def listCities(user_name, password, db_name, state_name):
-    db = MySQLdb.connect(user=user_name, passwd=password, db=db_name)
-    cur = db.cursor()
-    query = """
-    SELECT cities.name
-    FROM cities
-    LEFT JOIN states ON cities.state_id = states.id
-    WHERE states.name LIKE BINARY %s
-    ORDER BY cities.id ASC
-    """
+    try:
+        db = MySQLdb.connect(user=user_name, passwd=password, db=db_name)
+        cur = db.cursor()
+        query = """
+        SELECT cities.name
+        FROM cities
+        INNER JOIN states ON cities.state_id = states.id
+        WHERE states.name LIKE BINARY %s
+        ORDER BY cities.id ASC
+        """
 
-    cur.execute(query, (state_name,))
-    state_rows = cur.fetchall()
+        cur.execute(query, (state_name,))
+        state_rows = cur.fetchall()
 
-    for state in state_rows:
-        print("".join(state), end=" ")
-    print()
+        for state in state_rows:
+            print("".join(state), end=" ")
+        print()
 
-    cur.close()
-    db.close()
+        cur.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("Error {}".format(e))
+        sys.exit(1)
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <db name> <state name>")
     [user_name, password, db_name, state_name] = sys.argv[1:]
     listCities(user_name, password, db_name, state_name)
