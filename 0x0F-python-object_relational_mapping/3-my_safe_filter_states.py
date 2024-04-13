fc3178b361baeb0fa06e32495953bf579ebfd86e
otienojoseph <otienoj.cruz@gmail.com>
2024-04-13 11:59:21 +0300

    Add 3-safe_filter_states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script that returns list of states from hbtn_0e_0_usa database"""
+
+import sys
+import MySQLdb
+
+
+def listStates(user_name, password, db_name, state):
+    """Script that returns all states whose names match state"""
+    db = MySQLdb.connect(user=user_name, passwd=password, db=db_name)
+    cur = db.cursor()
+    query = """
+    SELECT * FROM states
+    WHERE name LIKE BINARY %s
+    ORDER BY id ASC
+    """
+
+    cur.execute(query, (state,))
+    state_rows = cur.fetchall()
+
+    for state in state_rows:
+        print(state)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    [user_name, password, db_name, state] = sys.argv[1:]
+    listStates(user_name, password, db_name, state)
