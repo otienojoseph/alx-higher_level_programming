57982bd0865ba63ec170829c27a3b244bac7ec7a
otienojoseph <otienoj.cruz@gmail.com>
2024-04-15 01:14:58 +0300

    style: Add formatting

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -24,7 +24,8 @@ class City(Base):
     __tablename__ = "cities"
 
     id = Column(
-        Integer, primary_key=True, unique=True, nullable=False, autoincrement=True
+        Integer, primary_key=True, unique=True, nullable=False,
+        autoincrement=True
     )
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
