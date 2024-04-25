06a56bc0647d4e3bf1ac86a257cb3e33678620e7
otienojoseph <otienoj.cruz@gmail.com>
2024-04-25 23:44:06 +0300

    Initial commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script that takes in a URL, sends a request to that URL
-# and displays the size of the body of response
+# and displays the size of the body of response in bytes
 
 size=$(curl -s -o /dev/null -w "%{size_download}" "$@")
-echo "${size}"
+echo "$size"
