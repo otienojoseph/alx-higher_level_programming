97b602001727506e6a0e2b07469dbfb37329bf8c
otienojoseph <otienoj.cruz@gmail.com>
2024-04-27 00:12:10 +0300

    fix: Add check for status code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # sends a GET request to the URL, and display the response body
-curl -X "GET" "$@"
+response_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")
+
+if [[ "$response_code" -eq 200 ]]; then
+	curl -s "$1"
+fi
