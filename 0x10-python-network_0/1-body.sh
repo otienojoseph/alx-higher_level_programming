bb13a4b55ca6389a5e29d12c1fbf9d19cf60f953
otienojoseph <otienoj.cruz@gmail.com>
2024-04-27 01:26:45 +0300

    fix: Add following for redirects

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and display the response body
-response_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-
-if [[ "$response_code" -eq 200 ]]; then
-	curl -s "$1"
-fi
+curl -Ls "$1"
