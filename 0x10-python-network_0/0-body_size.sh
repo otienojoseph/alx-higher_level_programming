89966170deea3b43bbd7a728b9f9fb9647c66468
otienojoseph <otienoj.cruz@gmail.com>
2024-04-26 15:53:17 +0300

    fix: Make sure word cout is 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Script that takes in a URL, sends a request to that URL
-# and displays the size of the body of response in bytes
-
-size=$(curl -s -o /dev/null -w "%{size_download}" "$@")
-echo "$size"
+# Takes URL,sends a request to that URL and prints size of response
+echo "$(curl -s -o /dev/null -w "%{size_download}" "$@")"
